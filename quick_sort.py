613377a7e13f7033c5bc46619cf820b0c9ea347a
VARTHYA SRIKANTH (202130) <varthya.srikanthnayak@gmail.com>
2022-09-12 16:48:55 +0530

    Made chabges in quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -15,24 +15,18 @@ def pivot(list, start, end):
             low = low + 1
  
         if low <= high:
- 
 # Swapping values to rearrange
             list[low], list[high] = list[high], list[low]
-          
         else:
             break
- 
 # Swapping pivot with high so that pivot is at its right # #position 
     list[start], list[high] = list[high], list[start]
- 
-# Returning pivot position
     return high
  
 # Function to do quick sort
 def quick_sort(list, start, end):
     if start >= end:
         return
- 
 #call pivot element
     p = pivot(list, start, end)
     quick_sort(list, start, p-1)
@@ -40,6 +34,5 @@ def quick_sort(list, start, end):
  
 # Driver code 
 myList = [45,22,13,4,78,11,51,10,3]
- 
 quick_sort(myList, 0, len(myList) - 1)
 print(myList)
